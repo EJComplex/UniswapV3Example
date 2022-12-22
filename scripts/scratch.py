9b97428f46c067afcb7d1748b8db6fe959d4b172
[Erik James] <[ejames.engineering@gmail.com]>
2022-12-22 16:18:43 -0600

    Finished UniswapV3 pool fee analysis. Code not cleaned yet.

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -1,21 +1,100 @@
 import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
+import openpyxl
+
+wb = openpyxl.load_workbook(r"./output/SingleSwapTesting.xlsx")
+dai_usdc = ["dai_usdc_3000", "dai_usdc_500", "dai_usdc_100"]
+dai_weth = ["dai_weth_3000", "dai_weth_500", "dai_weth_100"]
+
+fig = make_subplots(rows=1, cols=1, specs=[[{"secondary_y": True}]])
+for sheet in dai_usdc:
+    df = pd.read_excel(r"./output/SingleSwapTesting.xlsx", sheet_name=sheet)
+
+    fig.add_trace(
+        go.Scatter(
+            x=df["amountIn"],
+            y=df["impliedTokenOutValue"],
+            line=dict(width=2),
+            name="Pool Fee " + str(sheet.split("_")[2]),
+            connectgaps=True,
+        ),
+        secondary_y=False,
+    )
+
+    fig.update_layout(
+        title="Effects of Uniswap Pool Fee Selection",
+        xaxis_title="Dai Quantity In",
+        yaxis_title="Implied USDC Value ($)",
+    )
+
+    fig.write_html(r"./output/dai_usdc.html")
+
+fig = make_subplots(rows=1, cols=1, specs=[[{"secondary_y": True}]])
+for sheet in dai_weth:
+    df = pd.read_excel(r"./output/SingleSwapTesting.xlsx", sheet_name=sheet)
+
+    fig.add_trace(
+        go.Scatter(
+            x=df["amountIn"],
+            y=df["impliedTokenOutValue"],
+            line=dict(width=2),
+            name="Pool Fee " + str(sheet.split("_")[2]),
+            connectgaps=True,
+        ),
+        secondary_y=False,
+    )
+
+    fig.update_layout(
+        title="Effects of Uniswap Pool Fee Selection",
+        xaxis_title="Dai Quantity In",
+        yaxis_title="Implied Weth Value($)",
+    )
+
+    fig.write_html(r"./output/dai_weth.html")
 
 
-swapConfig = pd.read_csv(r"config/single_swap_config.csv")
+# for sheet in wb.sheetnames:
 
+#     df = pd.read_excel(r"./output/SingleSwapTesting.xlsx", sheet_name=sheet)
+#     fig = make_subplots(rows=1, cols=1, specs=[[{"secondary_y": True}]])
 
-print(swapConfig)
-for row in swapConfig.iterrows():
-    print(row)
-
-
-# for row in swapConfig.index:
-#     outputdf = singleTest(
-#         swapConfig.loc[row, ["Token In"]],
-#         swapConfig.loc[row, ["Token Out"]],
-#         swapConfig.loc[row, ["Token In Units"]],
-#         swapConfig.loc[row, ["Token Out Units"]],
-#         swapConfig.loc[row, ["Pool Fee"]],
+#     fig.add_trace(
+#         go.Scatter(
+#             x=df["amountIn"],
+#             y=df["amountOut"],
+#             line=dict(color="blue", width=2),
+#             name=sheet,
+#             connectgaps=True,
+#         ),
+#         secondary_y=False,
 #     )
 
-# outputdf.to_csv(r"output\SingleSwapTesting.csv")
+#     fig.add_trace(
+#         go.Scatter(
+#             x=df["amountIn"],
+#             y=df["impliedTokenOutValue"],
+#             line=dict(color="red", width=2),
+#             name="Implied Value",
+#             connectgaps=True,
+#         ),
+#         secondary_y=True,
+#     )
+
+#     # fig.add_trace(go.Scatter(x = df["rex"], y = [0.38]*len(df),line=dict(color='orange', width=2,dash='dash'), name = "Optimal OEE", connectgaps=True ),secondary_y=False)
+
+#     # fig.update_traces(marker=dict(size=3,))
+#     fig.update_layout(
+#         title="Effects of Uniswap Pool Fee Selection",
+#         xaxis_title=sheet + " Quantity In",
+#         yaxis_title=sheet + " Quantity Out",
+#     )
+#     y_title = "tokenIn/tokenOut"
+#     # fig.update_layout(yaxis_title=dict(text=y_title,font=dict(color='#FF5000',size=16,family="Source Sans Pro")),secondary_y=True)
+#     fig.update_yaxes(title_text=y_title, secondary_y=True)
+
+#     # fig = FormatDisplay(fig,"St Mary's AHU Heating Valve % vs Outside Air Temp","OAT Degrees F","Valve %")
+#     # fig = FormatDisplay(fig,"OEE per Master Rex", "Master Rex", "OEE", "Quantity [Gallons]")
+
+#     fig.write_html(r"./output/" + sheet + ".html")
--- scripts/swap_fee_analysis.py
+++ scripts/swap_fee_analysis.py
@@ -12,6 +12,42 @@ import time
 from web3 import Web3
 import pandas as pd
 import numpy as np
+import os
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
+
+# Decorator to print balance before and after transaction.
+# ETH by default, token if given.
+def balanceIs(account, token=None, units="ether"):
+    def balanceIsInner(func):
+        def wrap(*args, **kwargs):
+            if not token:
+                print(Web3.fromWei(account.balance(), units))
+            if token:
+                token_address = config["networks"][network.show_active()][token]
+                token_contract = interface.IERC20(token_address)
+                print(
+                    f"{token} balance is {Web3.fromWei(token_contract.balanceOf(account.address), units)}"
+                )
+
+            result = func(*args, **kwargs)
+
+            if not token:
+                print(Web3.fromWei(account.balance(), units))
+            if token:
+                token_address = config["networks"][network.show_active()][token]
+                token_contract = interface.IERC20(token_address)
+                print(
+                    f"{token} balance is {Web3.fromWei(token_contract.balanceOf(account.address), units)}"
+                )
+
+            return result
+
+        return wrap
+
+    return balanceIsInner
+
 
 # Deploy swap contract
 def deploy(contract, swapRouter):
@@ -22,6 +58,9 @@ def deploy(contract, swapRouter):
 
 
 # Perform swap
+@balanceIs(get_account(index=-2), token="dai")
+@balanceIs(get_account(index=-2), token="usdc", units="mwei")
+@balanceIs(get_account(index=-2), token="weth")
 def singleSwap(swapContract, tokenIn, tokenOut, amountIn, poolFee):
     account = get_account(index=-2)
     tx = swapContract.swapExactInputSingle(
@@ -96,7 +135,7 @@ def singleTest(_tokenIn, _tokenOut, _unitsIn, _unitsOut, _poolFee):
     swapContract = deploy(UniswapV3Swap, swapRouter)
 
     # define inputs
-    print(_tokenOut)
+    # print(_tokenOut)
     tokenOut = config["networks"][network.show_active()][_tokenOut]
     tokenIn = config["networks"][network.show_active()][_tokenIn]
     unitsOut = _unitsOut
@@ -110,37 +149,53 @@ def singleTest(_tokenIn, _tokenOut, _unitsIn, _unitsOut, _poolFee):
     # Call approve token function
     max_amount = Web3.toWei(2**64 - 1, "ether")
     approveToken(tokenIn, swapContract.address, max_amount)
+    approveToken(tokenOut, swapContract.address, max_amount)
 
     # Call swap function
-    token_contract = interface.IERC20(tokenOut)
+    token_contract_out = interface.IERC20(tokenOut)
+    token_contract_in = interface.IERC20(tokenIn)
     outputList = []
     inputList = []
     inputReverseList = []
     outputReverseList = []
     starting_balance = float(
-        Web3.fromWei(token_contract.balanceOf(account.address), unitsOut)
+        Web3.fromWei(token_contract_out.balanceOf(account.address), unitsOut)
     )
-    starting_balance_reverse = float(
-        Web3.fromWei(token_contract.balanceOf(account.address), unitsIn)
-    )
-    for i in range(1, 11):
+
+    for i in range(1, 21):
         amountIn = i * Web3.toWei(100, unitsIn)
         # amountIn = 100692897436421072274
 
+        print(
+            "Swapping "
+            + str(Web3.fromWei(amountIn, unitsIn))
+            + " "
+            + _tokenIn
+            + " for "
+            + _tokenOut
+        )
         tx = singleSwap(swapContract, tokenIn, tokenOut, amountIn, poolFee)
         outputList.append(
-            float(Web3.fromWei(token_contract.balanceOf(account.address), unitsOut))
+            float(Web3.fromWei(token_contract_out.balanceOf(account.address), unitsOut))
             - starting_balance
         )
 
-        inputList.append(amountIn)
+        inputList.append(Web3.fromWei(amountIn, unitsIn))
 
+        print("Swapping " + str(outputList[-1]) + " " + _tokenOut + " for " + _tokenIn)
+        starting_balance_reverse = float(
+            Web3.fromWei(token_contract_in.balanceOf(account.address), unitsIn)
+        )
         tx_reverse = singleSwap(
-            swapContract, tokenOut, tokenIn, outputList[-1], poolFee
+            swapContract,
+            tokenOut,
+            tokenIn,
+            Web3.toWei(outputList[-1], unitsOut),
+            poolFee,
         )
 
         outputReverseList.append(
-            float(Web3.fromWei(token_contract.balanceOf(account.address), unitsIn))
+            float(Web3.fromWei(token_contract_in.balanceOf(account.address), unitsIn))
             - starting_balance_reverse
         )
 
@@ -151,7 +206,10 @@ def singleTest(_tokenIn, _tokenOut, _unitsIn, _unitsOut, _poolFee):
     df["amountOut"] = outputList.copy()
     df["amountInReverse"] = inputReverseList.copy()
     df["amountOutReverse"] = outputReverseList.copy()
-    df["pool" + _tokenIn + "-" + _tokenOut] = [str(pool)] * len(df)
+    df["impliedTokenOutValue"] = df["amountIn"].astype(float) / df["amountOut"].astype(
+        float
+    )
+    df["pool " + _tokenIn + "-" + _tokenOut] = [str(pool)] * len(df)
 
     return df.copy()
 
@@ -163,6 +221,12 @@ def transferToken(token, contractAddress, amount):
     return tx
 
 
+def ToExcel(Dict: dict, path: str, file: str):
+    with pd.ExcelWriter(os.path.join(path, file)) as writer:
+        for key, df in Dict.items():
+            df.to_excel(writer, sheet_name=key)
+
+
 def main():
     # Deploy swap contract
     # based on config file swap selected tokens
@@ -171,24 +235,27 @@ def main():
     # create dataframe with this info
     # create plot to display pool imbalances
 
+    # Add some Weth to account for slippage
+    account = get_account(index=-2)
+    value = Web3.toWei(1, "ether")
+    tx = interface.IWETH9(config["networks"][network.show_active()]["weth"]).deposit(
+        {"from": account, "value": value}
+    )
+
     swapConfig = pd.read_csv(r"config/single_swap_config.csv")
 
-    for row in swapConfig.iterrows():
+    outputDict = {}
+    for index, row in swapConfig.iterrows():
         outputdf = singleTest(
-            row[1],
-            row[2],
-            row[3],
-            row[4],
-            row[0],
+            row["Token In"],
+            row["Token Out"],
+            row["Token In Units"],
+            row["Token Out Units"],
+            row["Pool Fee"],
         )
+        outputDict[
+            row["Token In"] + "_" + row["Token Out"] + "_" + str(row["Pool Fee"])
+        ] = outputdf.copy()
 
-    # for row in swapConfig.index:
-    #     outputdf = singleTest(
-    #         swapConfig.loc[row, ["Token In"]],
-    #         swapConfig.loc[row, ["Token Out"]],
-    #         swapConfig.loc[row, ["Token In Units"]],
-    #         swapConfig.loc[row, ["Token Out Units"]],
-    #         swapConfig.loc[row, ["Pool Fee"]],
-    #     )
-
-    outputdf.to_csv(r"output\SingleSwapTesting.csv")
+    # Save output to excel
+    ToExcel(outputDict, r"./output/", "SingleSwapTesting.xlsx")
